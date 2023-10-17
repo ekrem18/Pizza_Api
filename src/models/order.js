cae653fb34916341cd97f98738d362763b8fb383
Ekrem <avekremyilmazturk@gmail.com>
2023-10-17 20:49:32 +0300

    up

---
+++ src/models/order.js
@@ -0,0 +1,49 @@
+"use strict"
+
+const {mongoose} = require('../configs/dbConnection')
+/* ------------------------------------------------------- */
+// Order Model:
+
+const OrderSchema = new mongoose.Schema({
+    userId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true,
+    },
+    
+    pizzaId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Pizza',
+        required: true,
+    },
+    
+    size : {
+        type: String,
+        trim: true,
+        required: true,
+        enum:['Small', 'Medium', 'Large', 'XLarge']
+    },
+   
+    quantity : {
+        type: Number,
+        required: true,
+    },
+
+    price : {
+        type: Number,
+        required: true,
+    },
+    
+    totalPrice : {
+        type: Number,
+    },
+
+}, {
+    collection: 'orders',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
+
+
+
