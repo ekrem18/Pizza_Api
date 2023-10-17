0e63960c89843fa05c34434fc612b375f6b53f82
Ekrem <avekremyilmazturk@gmail.com>
2023-10-17 20:11:15 +0300

    toppingschema

---
+++ src/models/topping.js
@@ -0,0 +1,23 @@
+"use strict"
+
+const {mongoose} = require('../configs/dbConnection')
+/* ------------------------------------------------------- */
+// Topping Model:
+
+const ToppingSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+
+}, {
+    collection: 'toppings',
+    timestamps: true
+})
+
+module.exports = mongoose.model('Topping', ToppingSchema)
+
+
+
