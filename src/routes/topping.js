f3041e27e8b80afeff1c9f918294017fa16a8ce5
Ekrem <avekremyilmazturk@gmail.com>
2023-10-17 20:16:14 +0300

    route

---
+++ src/routes/topping.js
@@ -0,0 +1,21 @@
+"use strict"
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+// routes/topping:
+const topping = require('../controllers/topping')
+//URL:/toppings
+
+router.route('/')
+    .get(topping.list)
+    .post(topping.create)
+
+router.route('/:id')
+    .get(topping.read)
+    .put(topping.update)
+    .patch(topping.update)
+    .delete(topping.delete)
+
+
+
+/* ------------------------------------------------------- */
+module.exports = router
